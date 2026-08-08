0176767f5e984e4340f3b45b8e91b7ae1d6c39b5
Alishov Raul <alishov7394@gmail.com>
2026-08-08 13:47:29 +0400

    Task 3: Select salaty * 12 with alisas

--- Оператор SELECT/SELECT_small_practice.sql
+++ Оператор SELECT/SELECT_small_practice.sql
@@ -3,5 +3,12 @@ SELECT *
 FROM employees;
 
 -- Task 2: Select first_name, last_name, salary from employees table
-SELECT first_name, last_name, salary
+SELECT first_name,
+       last_name,
+       salary
+FROM employees;
+
+-- Task 3: Select salaty * 12 with alisas
+SELECT first_name AS name,
+       salary * 12 AS annual_salary
 FROM employees;
