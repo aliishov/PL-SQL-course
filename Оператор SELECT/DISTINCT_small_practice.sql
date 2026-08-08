a6baf4973b104ab193c3e6df2b715dae446cf8da
Alishov Raul <alishov7394@gmail.com>
2026-08-08 14:59:52 +0400

    Task 2: Select unique job_id and department_id combination from job_history table

--- Оператор SELECT/DISTINCT_small_practice.sql
+++ Оператор SELECT/DISTINCT_small_practice.sql
@@ -1,3 +1,7 @@
 -- Task 1: Select unique job_id from job_history table
 SELECT DISTINCT job_id
 FROM job_history;
+
+-- Task 2: Select unique job_id and department_id combination from job_history table
+SELECT DISTINCT job_id, department_id
+FROM job_history;
