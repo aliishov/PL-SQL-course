1d43b6acb14332b5a4528d7450a2cb663e057fc8
Alishov Raul <alishov7394@gmail.com>
2026-08-08 12:14:14 +0400

    Task 2: Initialize variables and print them using DBMS_OUTPUT

--- Типы Данных/Data_types_small_practice.sql
+++ Типы Данных/Data_types_small_practice.sql
@@ -8,5 +8,21 @@ DECLARE
     v_hire_date   DATE;
     v_is_active   BOOLEAN;
 BEGIN
-
+-- Task 2: Initialize variables and print them using DBMS_OUTPUT
+    v_employee_id := 100;
+    v_first_name := 'Raul';
+    v_salary := 1850.00;
+    v_hire_date := TO_DATE('2026-08-10', 'YYYY-MM-DD');
+    v_is_active := TRUE;
+    
+    DBMS_OUTPUT.PUT_LINE('ID: ' || v_employee_id);
+    DBMS_OUTPUT.PUT_LINE('Name: ' || v_first_name);
+    DBMS_OUTPUT.PUT_LINE('Salary: ' || v_salary);
+    DBMS_OUTPUT.PUT_LINE('Hire date: ' || TO_CHAR(v_hire_date, 'DD.MM.YYYY'));
+    
+    IF v_is_active THEN
+        DBMS_OUTPUT.PUT_LINE('Is active: TRUE');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Is active: FALSE');
+    END IF;
 END;
