669f139d4cf05ec8afed4e21487aa9b438577dc8
Alishov Raul <alishov7394@gmail.com>
2026-08-08 13:45:41 +0400

    Task 2: Select first_name, last_name, salary from employees table

--- Оператор SELECT/SELECT_small_practice.sql
+++ Оператор SELECT/SELECT_small_practice.sql
@@ -1,3 +1,7 @@
 -- Task 1: Select all columns from employees table
 SELECT *
 FROM employees;
+
+-- Task 2: Select first_name, last_name, salary from employees table
+SELECT first_name, last_name, salary
+FROM employees;
