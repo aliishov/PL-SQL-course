0b0265838c55babecbc2883983bfe648720328b0
Alishov Raul <alishov7394@gmail.com>
2026-08-09 13:26:12 +0400

    Task 2: Select first_name, salary and salary multiply by 12

--- Оператор SELECT/Expressions_small_practice.sql
+++ Оператор SELECT/Expressions_small_practice.sql
@@ -1,3 +1,10 @@
--- Task 1: Select alasry multiply by 12
-SELECT salary * 12
+-- Task 1: Select salary and salary multiply by 12
+SELECT salary, 
+       salary * 12
+FROM employees;
+
+-- Task 2: Select first_name, salary and salary multiply by 12
+SELECT first_name,
+       salary, 
+       salary * 12
 FROM employees;
