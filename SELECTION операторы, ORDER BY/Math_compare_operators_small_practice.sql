46b40e6d70841d5a867097cee3ae8d531c7cc3fb
Alishov Raul <alishov7394@gmail.com>
2026-08-16 10:36:48 +0400

    Task 2: Print first_name and last_name for employees with a salary less than 10,000

--- SELECTION операторы, ORDER BY/Math_compare_operators_small_practice.sql
+++ SELECTION операторы, ORDER BY/Math_compare_operators_small_practice.sql
@@ -4,3 +4,8 @@ SELECT first_name,
 FROM employees
 WHERE salary > 10000;
 
+-- Task 2: Print first_name and last_name for employees with a salary less than 10,000.
+SELECT first_name,
+       last_name
+FROM employees
+WHERE salary < 10000;
