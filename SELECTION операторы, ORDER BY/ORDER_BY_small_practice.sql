7957aa2e699adcbcad1632b8788fd14b2097063f
Alishov Raul <alishov7394@gmail.com>
2026-08-22 15:01:28 +0400

    Task 3: Print the first_name, salary, and hire_date for employees with job_id = 'IT_PROG'. Sort by hire_date

--- SELECTION операторы, ORDER BY/ORDER_BY_small_practice.sql
+++ SELECTION операторы, ORDER BY/ORDER_BY_small_practice.sql
@@ -9,3 +9,12 @@ SELECT first_name,
        salary
 FROM employees
 ORDER BY salary DESC;
+
+-- Task 3: Print the first_name, salary, and hire_date for employees with job_id = 'IT_PROG'. Sort by hire_date.
+SELECT first_name,
+       salary,
+       hire_date
+FROM employees
+WHERE job_id = 'IT_PROG'
+ORDER BY hire_date;
+
