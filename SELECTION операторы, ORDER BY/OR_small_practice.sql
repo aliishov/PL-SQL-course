85f4331f0999800e241e7ad340d66f0d20e502dd
Alishov Raul <alishov7394@gmail.com>
2026-08-22 14:45:53 +0400

    Task 4: Display employees with a hire_date of 2005-01-01 or a hire_date of 2006-01-01. Use TO_DATE

--- SELECTION операторы, ORDER BY/OR_small_practice.sql
+++ SELECTION операторы, ORDER BY/OR_small_practice.sql
@@ -16,3 +16,10 @@ FROM employees
 WHERE first_name = 'Steven' 
    OR first_name = 'David';
    
+-- Task 4: Display employees with a hire_date of 2005-01-01 or 
+--         a hire_date of 2006-01-01. Use TO_DATE.
+SELECT * 
+FROM employees
+WHERE hire_date = TO_DATE('2005-01-01', 'yyyy-mm-dd') 
+   OR hire_date = TO_DATE('2006-01-01', 'yyyy-mm-dd');
+.
