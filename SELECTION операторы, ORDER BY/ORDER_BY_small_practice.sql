e0aae50a3a4299897ed39c397212f002a08d6d60
Alishov Raul <alishov7394@gmail.com>
2026-08-22 14:59:33 +0400

    Task 2: Print the first_name and salary of employees. Sort by salary descending

--- SELECTION операторы, ORDER BY/ORDER_BY_small_practice.sql
+++ SELECTION операторы, ORDER BY/ORDER_BY_small_practice.sql
@@ -3,3 +3,9 @@ SELECT first_name,
        salary
 FROM employees
 ORDER BY salary;
+
+-- Task 2: Print the first_name and salary of employees. Sort by salary descending.
+SELECT first_name,
+       salary
+FROM employees
+ORDER BY salary DESC;
