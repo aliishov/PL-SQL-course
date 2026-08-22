5d020df5484fd98c09b83e961cf03d12cb1a1a97
Alishov Raul <alishov7394@gmail.com>
2026-08-22 14:43:51 +0400

    Task 2: Display employees with a job_id of 'IT_PROG' or a job_id of 'ST_CLERK'

--- SELECTION операторы, ORDER BY/OR_small_practice.sql
+++ SELECTION операторы, ORDER BY/OR_small_practice.sql
@@ -3,3 +3,9 @@ SELECT *
 FROM employees
 WHERE department_id = 60 
    OR department_id = 90;
+
+-- Task 2: Display employees with a job_id of 'IT_PROG' or a job_id of 'ST_CLERK'.
+SELECT * 
+FROM employees
+WHERE job_id = 'IT_PROG' 
+   OR job_id = 'ST_CLERK';
