2d8716a43168c6e8122692135b1f9597481dd16c
Alishov Raul <alishov7394@gmail.com>
2026-08-22 15:02:49 +0400

    Task 4: Print the salary and hire_date for employees with job_id = 'IT_PROG'. Sort by first_name, even if first_name is not in the SELECT list

--- SELECTION операторы, ORDER BY/ORDER_BY_small_practice.sql
+++ SELECTION операторы, ORDER BY/ORDER_BY_small_practice.sql
@@ -10,7 +10,8 @@ SELECT first_name,
 FROM employees
 ORDER BY salary DESC;
 
--- Task 3: Print the first_name, salary, and hire_date for employees with job_id = 'IT_PROG'. Sort by hire_date.
+-- Task 3: Print the first_name, salary, and hire_date for employees with 
+--         job_id = 'IT_PROG'. Sort by hire_date.
 SELECT first_name,
        salary,
        hire_date
@@ -18,3 +19,10 @@ FROM employees
 WHERE job_id = 'IT_PROG'
 ORDER BY hire_date;
 
+-- Task 4: Print the salary and hire_date for employees with job_id = 'IT_PROG'. 
+--         Sort by first_name, even if first_name is not in the SELECT list.
+SELECT salary,
+       hire_date
+FROM employees
+WHERE job_id = 'IT_PROG'
+ORDER BY first_name;
