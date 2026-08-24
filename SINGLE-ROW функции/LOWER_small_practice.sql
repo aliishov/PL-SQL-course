abb81a5e47e5178b281324fe97344f4fff8f0656
Raul Alishov <alishov7394@gmail.com>
2026-08-24 12:43:18 +0400

    Task 2: Print last_name and LOWER(last_name) from employees

--- SINGLE-ROW функции/LOWER_small_practice.sql
+++ SINGLE-ROW функции/LOWER_small_practice.sql
@@ -2,3 +2,8 @@
 SELECT first_name
        LOWER(first_name) AS first_name_lower
 FROM employees;
+
+-- Task 2: Print last_name and LOWER(last_name) from employees.
+SELECT last_name
+       LOWER(last_name) AS last_name_lower
+FROM employees;
