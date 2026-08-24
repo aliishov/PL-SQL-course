84ed75d356db22ed5645b67505e8f5bdf7a862e6
Raul Alishov <alishov7394@gmail.com>
2026-08-24 12:55:33 +0400

    Task 2: Print last_name and UPPER(last_name) from employees

--- SINGLE-ROW функции/UPPER_small_practice.sql
+++ SINGLE-ROW функции/UPPER_small_practice.sql
@@ -3,3 +3,8 @@ SELECT first_name
        UPPER(first_name) AS first_name_lower
 FROM employees;
 
+-- Task 2: Print last_name and UPPER(last_name) from employees.
+SELECT last_name
+       UPPER(last_name) AS last_name_lower
+FROM employees;
+
