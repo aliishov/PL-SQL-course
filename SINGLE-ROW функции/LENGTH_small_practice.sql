20fe047b7ef6127f7ee9d2e25c0e0ceef2f3de4d
Raul Alishov <alishov7394@gmail.com>
2026-08-25 14:18:09 +0400

    Task 2: Print last_name and LENGTH(last_name) from employees

--- SINGLE-ROW функции/LENGTH_small_practice.sql
+++ SINGLE-ROW функции/LENGTH_small_practice.sql
@@ -1,4 +1,9 @@
 -- Task 1: Print first_name and LENGTH(first_name) from employees.
 SELECT first_name,
-       LENGTH(first_name) AS length
+       LENGTH(first_name) AS lngth
+FROM employees;
+
+-- Task 2: Print last_name and LENGTH(last_name) from employees.
+SELECT last_name,
+       LENGTH(last_name) AS lngth
 FROM employees;
