90fffb527974637e4199604f60f3b00dc4a497cd
Raul Alishov <alishov7394@gmail.com>
2026-08-25 14:29:51 +0400

    Task 3: Print LPAD('25', 5, '0') from dual

--- SINGLE-ROW функции/LPAD_RPAD_small_practice.sql
+++ SINGLE-ROW функции/LPAD_RPAD_small_practice.sql
@@ -5,3 +5,7 @@ FROM dual;
 -- Task 2: Print RPAD('Oracle', 10, '*') from dual.
 SELECT RPAD('Oracle', 10, '*')
 FROM dual;
+
+-- Task 3: Print LPAD('25', 5, '0') from dual.
+SELECT LPAD('25', 5, '0')
+FROM dual;
