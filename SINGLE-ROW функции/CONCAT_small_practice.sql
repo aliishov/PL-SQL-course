82930d7153834d9f5ee8117c757448d724ff3248
Raul Alishov <alishov7394@gmail.com>
2026-08-25 14:10:37 +0400

    Task 5: Concatenate 3 pieces: 'Privet' 'my' 'druq'. Use nested CONCAT

--- SINGLE-ROW функции/CONCAT_small_practice.sql
+++ SINGLE-ROW функции/CONCAT_small_practice.sql
@@ -5,7 +5,7 @@ FROM dual;
 -- Task 2: Print first_name, last_name, and full_name using nested CONCAT.
 SELECT first_name,
        last_name,
-       CONCAT(first_name, CONCAT(' ', last_name) AS full_name
+       CONCAT(first_name, CONCAT(' ', last_name)) AS full_name
 FROM employees;
 
 -- Task 3: Print first_name, salary, and the text: Salary: salary. Use CONCAT.
@@ -16,4 +16,8 @@ FROM employees;
 
 -- Task 4: Print: Today is: SYSDATE using CONCAT.
 SELECT CONCAT('Today is: ', SYSDATE) AS today
+FROM dual;
+
+-- Task 5: Concatenate 3 pieces: 'Privet' 'my' 'druq'. Use nested CONCAT.
+SELECT CONCAT('Privet ', CONCAT('my', ' druq')) AS message
 FROM dual;
