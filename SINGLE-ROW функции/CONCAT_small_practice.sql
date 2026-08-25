094721769a1a97d4bc2a5c8b7595507d19808154
Raul Alishov <alishov7394@gmail.com>
2026-08-25 14:05:18 +0400

    Task 2: Print first_name, last_name, and full_name using nested CONCAT

--- SINGLE-ROW функции/CONCAT_small_practice.sql
+++ SINGLE-ROW функции/CONCAT_small_practice.sql
@@ -1,3 +1,9 @@
 -- Task 1: Concatenate 'Hello' and 'Oracle' using CONCAT.
 SELECT CONCAT('Hello', ' Oracle') AS message
 FROM dual;
+
+-- Task 2: Print first_name, last_name, and full_name using nested CONCAT.
+SELECT first_name,
+       last_name,
+       CONCAT(first_name, CONCAT(' ', last_name) AS full_name
+FROM employees;
