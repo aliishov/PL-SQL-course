4ad4fa3496000c2a30f788fa01861f90cc62e853
Raul Alishov <alishov7394@gmail.com>
2026-08-26 10:59:34 +0400

    Task 3: Check that the result is 0

--- SINGLE-ROW функции/INSTR_small_practice.sql
+++ SINGLE-ROW функции/INSTR_small_practice.sql
@@ -5,3 +5,8 @@ FROM dual;
 -- Task 2: Find the position of the text 'Dev'.
 SELECT INSTR('SQL Developer', 'Dev') AS result
 FROM dual;
+
+-- Task 3: Check that the result is 0.
+SELECT INSTR('SQL Developer', 'Java') AS result
+FROM dual;
+
