57e6c4ac458ab402e82a282673ba6a755068bc56
Raul Alishov <alishov7394@gmail.com>
2026-08-26 11:10:35 +0400

    Task 2: Get the text from position 4 to the end

--- SINGLE-ROW функции/SUBSTR_small_practice.sql
+++ SINGLE-ROW функции/SUBSTR_small_practice.sql
@@ -1,3 +1,7 @@
 -- Task 1: Get the first 3 characters.
-SELECT SUBSTR('Oracle', 1, 3)
+SELECT SUBSTR('Oracle', 1, 3) AS result
+FROM dual;
+
+-- Task 2: Get the text from position 4 to the end.
+SELECT SUBSTR('Oracle Database', 4) AS result
 FROM dual;
