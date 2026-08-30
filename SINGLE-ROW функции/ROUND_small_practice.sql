27905beb1f3eacc968eb5f8d686dd2ea737a8b5a
Alishov Raul <alishov7394@gmail.com>
2026-08-30 12:12:52 +0400

    Task 2: Round to 1 digit after decimal point

--- SINGLE-ROW функции/ROUND_small_practice.sql
+++ SINGLE-ROW функции/ROUND_small_practice.sql
@@ -1,3 +1,7 @@
 -- Task 1: Round to the nearest whole number.
 SELECT ROUND(46.67) AS result
 FROM dual;
+
+-- Task 2: Round to 1 digit after decimal point.
+SELECT ROUND(46.67, 1) AS result
+FROM dual;
