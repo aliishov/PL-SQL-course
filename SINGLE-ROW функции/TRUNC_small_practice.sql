b3e787d676bc6c851cb32f762078a1ef7427367c
Alishov Raul <alishov7394@gmail.com>
2026-08-30 12:35:00 +0400

    Task 2: Leave 1 digit after the decimal point

--- SINGLE-ROW функции/TRUNC_small_practice.sql
+++ SINGLE-ROW функции/TRUNC_small_practice.sql
@@ -3,8 +3,5 @@ SELECT TRUNC(46.67) AS result
 FROM dual;
 
 -- Task 2: Leave 1 digit after the decimal point.
--- Task 3: Leave 2 digits after the decimal point.
--- Task 4: Leave the number before tens.
--- Task 5: Leave the number before hundreds.
--- Task 6: Truncate salary to thousands.
--- Task 7: Truncate the calculated expression.
+SELECT TRUNC(46.67, 1) AS result
+FROM dual;
