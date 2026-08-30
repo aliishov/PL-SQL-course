8c25ec8ce81d5df6877ef1bfe0ef6b4c842da2db
Alishov Raul <alishov7394@gmail.com>
2026-08-30 11:58:14 +0400

    Task 2: Replace 'spaces' with 'underscores'

--- SINGLE-ROW функции/REPLACE_small_practice.sql
+++ SINGLE-ROW функции/REPLACE_small_practice.sql
@@ -1,3 +1,7 @@
 -- Task 1: Replace 'SQL' with 'PL/SQL'.
 SELECT REPLACE('Oracle SQL', 'SQL', 'PL/SQL') AS result
 FROM dual;
+
+-- Task 2: Replace 'spaces' with 'underscores'.
+SELECT REPLACE('Oracle SQL Course', ' ', '_') AS result
+FROM dual;
