864bd096c14f765b90072446261ecb24a59f6a18
Alishov Raul <alishov7394@gmail.com>
2026-09-02 10:49:22 +0400

    Task 2: Show current NLS_DATE_FORMAT

--- SINGLE-ROW функции/SYSDATE_small_practice.sql
+++ SINGLE-ROW функции/SYSDATE_small_practice.sql
@@ -1,3 +1,8 @@
 -- Task 1: Show current database date and time.
 SELECT SYSDATE AS server_date_time
 FROM dual;
+
+-- Task 2: Show current NLS_DATE_FORMAT.
+SELECT *
+FROM nls_session_parameters
+WHERE parameter = 'NLS_DATE_FORMAT';
