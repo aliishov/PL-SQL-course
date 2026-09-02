32074b1cb564f37a111822aaa828b8eab2a013dd
Alishov Raul <alishov7394@gmail.com>
2026-09-02 10:53:07 +0400

    Task 4: Add 10 days

--- SINGLE-ROW функции/SYSDATE_small_practice.sql
+++ SINGLE-ROW функции/SYSDATE_small_practice.sql
@@ -10,3 +10,7 @@ WHERE parameter = 'NLS_DATE_FORMAT';
 -- Task 3: Show SYSDATE with visible time part.
 SELECT TO_CHAR(SYSDATE, 'DD-MM-YYYY hh24:mi:ss') as server_date_time
 FROM dual;
+
+-- Task 4: Add 10 days.
+SELECT SYSDATE + 10 AS ten_days_later
+FROM dual;
