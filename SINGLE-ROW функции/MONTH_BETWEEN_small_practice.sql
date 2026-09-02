df65a3638cd4428d8e311859b4f20573e484519d
Alishov Raul <alishov7394@gmail.com>
2026-09-02 12:02:21 +0400

    Task 2: Reverse the dates

--- SINGLE-ROW функции/MONTH_BETWEEN_small_practice.sql
+++ SINGLE-ROW функции/MONTH_BETWEEN_small_practice.sql
@@ -1,3 +1,7 @@
 -- Task 1: Count months between two dates.
 SELECT MONTHS_BETWEEN(DATE '2020-05-01', DATE '2020-01-01') AS result
 FROM dual;
+
+-- Task 2: Reverse the dates.
+SELECT MONTHS_BETWEEN(DATE '2020-01-01', DATE '2020-05-01') AS result
+FROM dual;
