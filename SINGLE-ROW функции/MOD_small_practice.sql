01e08f5ea1d16cb8f2c4c420d07cccc8aa74069a
Alishov Raul <alishov7394@gmail.com>
2026-09-02 10:36:18 +0400

    Task 3: Check if the dividend is smaller than the divisor

--- SINGLE-ROW функции/MOD_small_practice.sql
+++ SINGLE-ROW функции/MOD_small_practice.sql
@@ -3,5 +3,9 @@ SELECT MOD(17, 5) AS result
 FROM dual;
 
 -- Task 2: Check the number without remainder.
+SELECT MOD(20, 5) AS result
+FROM dual;
+
+-- Task 3: Check if the dividend is smaller than the divisor.
 SELECT MOD(2, 5) AS result
 FROM dual;
