70d548bcf7556f21dbf37423beaebdea0f946c07
Alishov Raul <alishov7394@gmail.com>
2026-09-09 11:58:06 +0400

    Task 2: Find next Saturday after same Saturday

--- SINGLE-ROW функции/NEXT_DAY_small_practice.sql
+++ SINGLE-ROW функции/NEXT_DAY_small_practice.sql
@@ -1,3 +1,7 @@
 -- Task 1: Find next Monday after fixed date.
 SELECT NEXT_DAY(DATE '2026-09-09', 'MONDAY') AS result
 FROM dual;
+
+-- Task 2: Find next Saturday after same Saturday.
+SELECT NEXT_DAY(DATE '2026-09-05', 'SATURDAY') AS result
+FROM dual;
