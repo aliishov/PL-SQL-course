26fbabd7846bdd5f9e20290c9d17b971d3946478
Alishov Raul <alishov7394@gmail.com>
2026-09-09 13:11:00 +0400

    Task 2: Truncate fixed date to month

--- SINGLE-ROW функции/TRUNC_для_дат_small_practice.sql
+++ SINGLE-ROW функции/TRUNC_для_дат_small_practice.sql
@@ -1,3 +1,7 @@
 -- Task 1: Truncate current database date to day.
 SELECT TRUNC(SYSDATE) AS result
 FROM dual;
+
+-- Task 2: Truncate fixed date to month.
+SELECT TRUNC(DATE '2026-09-09', 'MM') AS result
+FROM dual;
