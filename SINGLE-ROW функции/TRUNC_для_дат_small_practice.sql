ba62e78d09e7ad57fa43a5caee724fe668de9391
Alishov Raul <alishov7394@gmail.com>
2026-09-09 13:11:35 +0400

    Task 3: Truncate end of month to month

--- SINGLE-ROW функции/TRUNC_для_дат_small_practice.sql
+++ SINGLE-ROW функции/TRUNC_для_дат_small_practice.sql
@@ -5,3 +5,7 @@ FROM dual;
 -- Task 2: Truncate fixed date to month.
 SELECT TRUNC(DATE '2026-09-09', 'MM') AS result
 FROM dual;
+
+-- Task 3: Truncate end of month to month.
+SELECT TRUNC(DATE '2026-12-31', 'MM') AS result
+FROM dual;
