b7ce2c917fc0ac7d770342a2d294356aa266bba8
Alishov Raul <alishov7394@gmail.com>
2026-09-09 12:53:09 +0400

    Task 2: Round day 15 to month

--- SINGLE-ROW функции/ROUND_для_дат_small_practice.sql
+++ SINGLE-ROW функции/ROUND_для_дат_small_practice.sql
@@ -1,3 +1,7 @@
 -- Task 1: Round fixed date to nearest day.
 SELECT ROUND(DATE ' 2026-09-09') AS result
 FROM dual;
+
+-- Task 2: Round day 15 to month.
+SELECT ROUND(DATE ' 2026-09-15', 'MM') AS result
+FROM dual;
