09b853ff10f840be9b749f0d6f8572795316b89a
Alishov Raul <alishov7394@gmail.com>
2026-09-09 12:25:51 +0400

    Task 2: Find last day of August 2026

--- SINGLE-ROW функции/LAST_DAY_small_practice.sql
+++ SINGLE-ROW функции/LAST_DAY_small_practice.sql
@@ -1,3 +1,7 @@
 -- Task 1: Find last day of current month.
 SELECT LAST_DAY(SYSDATE) AS result
 FROM dual;
+
+-- Task 2: Find last day of August 2026.
+SELECT LAST_DAY(DATE '2026-08-01') AS result
+FROM dual;
